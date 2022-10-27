7608ffd5d5bd25e96de613cc060b0c93174b1428
nkbadhon <badhon7574@gmail.com>
2022-10-27 16:12:08 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+
+const categories = require('./Data/datas.json');
+const { request } = require('express');
+
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Running')
+});
+
+app.get('/course-categories', (req, res) => {
+    res.send(categories)
+});
+
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = categories.find(c => c.id == id);
+    res.send(selectedCourse);
+});
+
+
+
+
